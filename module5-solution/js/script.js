c657bb0adfff271ba6e128ded009960c74fa3bd1
MeghanaS195 <38200315+MeghanaS195@users.noreply.github.com>
2022-04-29 12:11:36 +0530

    Update script.js

--- module5-solution/js/script.js
+++ module5-solution/js/script.js
@@ -98,8 +98,6 @@ function buildAndShowHomeHTML (categories) {
     homeHtmlUrl,
     function (homeHtml) {
 
-      alert(homeHtml);
-
       // TODO: STEP 2: Here, call chooseRandomCategory, passing it retrieved 'categories'
       // Pay attention to what type of data that function returns vs what the chosenCategoryShortName
       // variable's name implies it expects.
